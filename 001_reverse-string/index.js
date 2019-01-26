ffa94e610e4d90f6bf65a4456c5606ce9940aa09
Levblanc <levblanc@gmail.com>
2019-01-26 22:28:30 +0800

    feat: add exercises reverse string, palindrome, and fizz buzz

---
+++ 001_reverse-string/index.js
@@ -0,0 +1,62 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+// function reverse(str) {
+//   let tmp = []
+//   const strlen = str.length
+
+//   for (let i = 0; i < strlen; i ++) {
+//     tmp.unshift(str[i])  
+//   }
+
+//   return tmp.join('')
+// }
+
+// function reverse(str) {
+//   let tmp = ''
+//   const strlen = str.length
+
+//   for(let i = strlen - 1; i >= 0; i--) {
+//     tmp += str[i]
+//   }
+
+//   return tmp
+// }
+
+// function reverse(str) {
+//   const strlen = str.length
+//   let tmp = ''
+
+//   for(let i = strlen; i >= 0; i--) {
+//     tmp += str.slice(i - 1, i)
+//   }
+
+//   return tmp
+// }
+
+// function reverse(str) {
+//   return str.split('').reverse().join('')
+// }
+
+// function reverse(str) {
+//   let reversed = ''
+
+//   for (let char of str) {
+//     reversed = char + reversed
+//   }
+
+//   return reversed
+// }
+
+function reverse(str) {
+  return str.split('').reduce((rev, char) => char + rev, '')
+}
+
+// reverse('hello')
+
+module.exports = reverse;
